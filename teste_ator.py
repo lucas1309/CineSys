e0496471d3ca2c7000bd273fdaccea7c0ae0056d
JinSongLucasKong <lucaskong6@gmail.com>
2017-05-16 21:31:00 -0300

    Teste com dois atores
    implemantação com dois atores.

--- teste_ator.py
+++ teste_ator.py
@@ -20,3 +20,9 @@ class TestAtor(unittest.TestCase):
         self.assertEqual("Chris Hemsworth", a[1])
         self.assertEqual("Australiano", a[2])
         self.assertEqual(33, a[3])
+  def test_adicionar_dois_atores(self):
+        ator.adicionar_ator(2, "Jason Statham", "Ingles", 49)
+        ator.adicionar_ator(1, "Chris Hemsworth", "Australiano", 33)
+
+        atores = ator.listar_atores()
+        self.assertEqual(2, len(atores))
