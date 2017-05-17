b51ecac3f55df41c269b4fffa8497514880e3bca
JinSongLucasKong <lucaskong6@gmail.com>
2017-05-16 21:28:35 -0300

    Teste sem ator
    implementação do teste sem ator

--- teste_ator.py
+++ teste_ator.py
@@ -5,3 +5,6 @@ import ator
 class TestAtor(unittest.TestCase):
   def setUp(self):
         ator.remover_todos_atores()
+  def test_sem_ators(self):
+        atores = ator.listar_atores()
+        self.assertEqual(0, len(atores))
