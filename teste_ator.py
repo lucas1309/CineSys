832103f461b5f2452e479697ca5871cccf8d7afb
JinSongLucasKong <lucaskong6@gmail.com>
2017-05-16 21:29:18 -0300

    Teste com um ator
    Implementação com um ator.

--- teste_ator.py
+++ teste_ator.py
@@ -8,3 +8,15 @@ class TestAtor(unittest.TestCase):
   def test_sem_ators(self):
         atores = ator.listar_atores()
         self.assertEqual(0, len(atores))
+  def test_adicionar_um_ator(self):
+        ator.adicionar_ator(1, "Chris Hemsworth", "Australiano", 33)
+
+        atores = ator.listar_atores()
+        self.assertEqual(1, len(atores))
+
+        a = atores[0]
+
+        self.assertEqual(1, a[0])
+        self.assertEqual("Chris Hemsworth", a[1])
+        self.assertEqual("Australiano", a[2])
+        self.assertEqual(33, a[3])
